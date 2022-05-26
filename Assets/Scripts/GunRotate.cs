973799b21fa439d880b977c232e371b96cdd2e90
Kifor4ik <kifor4ik2.0@mail.ru>
2022-05-26 16:53:38 +0300

    v1.0.1-alpha
    just little fix of gun bug

--- Assets/Scripts/GunRotate.cs
+++ Assets/Scripts/GunRotate.cs
@@ -1,25 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class GunRotate : MonoBehaviour
 {
     
     //player and if need fix rotation by var offset
-    [SerializeField] private float offset;
     [SerializeField] private Rigidbody2D player;
     
     //gun image
     [SerializeField] private SpriteRenderer image;
+        //need to set layer
+    [SerializeField] private SpriteRenderer playerSprite;
     
     //bulet object and bullet start shooting pos
     [SerializeField] private GameObject bullet;
+
     [SerializeField] private Transform bulletPos;
     //to transform image
     private Transform playerTransform;
     private float correctY = 0.1F;
-    private float correctX = 0.03F;
+    private float correctX = 0.07F;
 
     [SerializeField] private float ShootingTemp = 0.3F;
     private float ShootingTempCurrent = 0.0F;
@@ -30,29 +33,31 @@ public class GunRotate : MonoBehaviour
     }
     
     void Update()
-    {
-        //Rotation
-        playerTransform = player.transform;
-        Vector3 rotate = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotateY = Mathf.Atan2(rotate.y,rotate.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f,0f ,rotateY + offset );
-        //Layer order and fixed up down position
-        this.transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y-0.07F, playerTransform.position.z);
-        if (
-            (Quaternion.Euler(0f, 0f, rotateY + offset).z < 0.96 && Quaternion.Euler(0f, 0f, rotateY + offset).z > 0.20) &&
-            (Quaternion.Euler(0f, 0f, rotateY + offset).w < 0.96 && Quaternion.Euler(0f, 0f, rotateY + offset).w > 0.20)
-        ) { image.sortingOrder = 0; correctY = 0.03F; correctX = 0.0f;}
-        else { image.sortingOrder = 2; correctY = 0.1f; correctX = 0.02f; }
-        //Layers
-        if (Quaternion.Euler(0f, 0f, rotateY + offset).z > -0.7 && Quaternion.Euler(0f, 0f, rotateY + offset).z < 0.70) image.flipY = false;
+    {    
+        // layer switch bone
+        if (playerSprite.sprite.name == "char 1_2" || playerSprite.sprite.name == "char 1_3" ||
+            playerSprite.sprite.name == "char 1_6")
+        {
+            image.sortingOrder = 0;
+            image.transform.position = transform.position = new Vector3(player.position.x, player.position.y);
+        }
+
         else
         {
-            
-            image.flipY = true;
+            image.sortingOrder = 2;
+            image.transform.position = transform.position = new Vector3(player.position.x, player.position.y - 0.1f);
+
         }
-        //Just move with player
-        this.transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y - correctY,
-            playerTransform.position.z);
+
+        Vector3 rotate = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotateY = Mathf.Atan2(rotate.y, rotate.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f,0f ,rotateY);
+        Debug.Log((Quaternion.Euler(0f, 0f, rotateY).z));
+
+        if (Quaternion.Euler(0f, 0f, rotateY).z > -0.7 && Quaternion.Euler(0f, 0f, rotateY).z < 0.70) image.flipY = false;
+        else image.flipY = true;
+        
+        
         //Shooting
         if (Input.GetMouseButton(0) && ShootingTempCurrent <= 0)
         {
